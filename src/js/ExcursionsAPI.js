21bd30d2dea63bb3315dd5f65d916b9b6d833512
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-04-05 16:54:32 +0200

    task part1

--- src/js/ExcursionsAPI.js
+++ src/js/ExcursionsAPI.js
@@ -1,5 +1,33 @@
 class ExcursionsAPI {
+    constructor() {
+
+    }
+
+    async addExcursion(excursionData) {
+        const excursions = await fetch('http://localhost:3000/excursions', {
+            method: 'GET',
+            headers: { 
+                'Content-Type': 'application/json'
+            }
+            
+        }).then((value) => value.json())
+        console.log(excursions);
+        
+        const id = excursions.length + 1
+        const data = {
+            ...excursionData, id
+        }
+
+        await fetch('http://localhost:3000/excursions', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    }
 
 }
+
 
 export default ExcursionsAPI;
--- src/js/client.js
+++ src/js/client.js
@@ -3,3 +3,11 @@ import './../css/client.css';
 import ExcursionsAPI from './ExcursionsAPI';
 
 console.log('client');
+
+const myClassX = new ExcursionsAPI();
+myClassX.addExcursion({
+    name: 'Ogrodzieniec',
+    description: "Lorem Ipsum",
+    adultPrice: 90,
+    childPrice: 50
+});
